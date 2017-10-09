e2f9dcc4b66dd01ec251f1632f7bbacca53c2271
seth <liyidong@yeezon.com>
2017-10-09 18:58:20 +0800

    array

---
+++ chapter12/array.js
@@ -0,0 +1,14 @@
+function CArray(numElements) {
+    this.dataStore = [];
+    this.pos = 0;
+    this.numElements = numElements;
+    this.insert = insert;
+    this.toString = toString;
+    this.clear = clear;
+    this.setData = setData;
+    this.swap = swap;
+
+    for(var i = 0; i < numElements; ++i) {
+        this.dataStore[i] = i;
+    }
+} 
